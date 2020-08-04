1e63d66d52235b2828c522c7ec895b80d5e4edd0
Peter Borkuti <peterborkuti@gmail.com>
2020-08-04 22:39:18 +0200

    upgrade to 10 - copy an empty generated angular project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NgConway
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.1.
 
 ## Development server
 
@@ -17,8 +17,6 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-*I made tests without karma. Install jasmine-ts and `npm test`. Tests for pure js classes are in the spec folder.*
 
 ## Running end-to-end tests
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GridComponent } from './grid/grid.component';
-import { IterationSpeedComponent } from './iteration-speed/iteration-speed.component';
-
-import { MatSliderModule } from '@angular/material/slider';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GridComponent,
-    IterationSpeedComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, MatSliderModule, BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
